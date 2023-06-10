1da8d79e81c3403a36f8b45cd4e9c2a28be9af1f
gulshan07dev <gulshanku9135@gmail.com>
2023-06-10 23:17:51 +0530

    adding preloader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -7,15 +7,27 @@ import Projects from "./components/projects";
 import Services from "./components/services";
 import Contact from "./components/contact";
 import Footer from "./components/footer";
-import './css/style.css';
-import './css/utilityclass.css';
-import './css/blobz.min.css';
+import Preloader from "./components/preloader";
+import "./css/style.css";
+import "./css/utilityclass.css";
+import "./css/blobz.min.css";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating a delay to showcase the preloader
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <Router>
-      <>
-        <Header />
+      <Header />
+      {isLoading ? (
+        <Preloader />
+      ) : (
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/skills" element={<Skills />} />
@@ -23,8 +35,8 @@ function App() {
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-        <Footer />
-      </>
+      )}
+      <Footer />
     </Router>
   );
 }
